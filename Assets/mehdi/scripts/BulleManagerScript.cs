a82ac3739653f0c43d6205faaf7d599390175b7d
merochd <33989674+merochd@users.noreply.github.com>
2025-01-29 21:57:44 +0100

    geysere + poisson + bulle fonctionnel
    bull à update: cliquable par souris

--- Assets/mehdi/scripts/BulleManagerScript.cs
+++ Assets/mehdi/scripts/BulleManagerScript.cs
@@ -4,17 +4,27 @@ namespace mehdi.scripts
 {
     public class BulleManagerScript : MonoBehaviour
     {
-        public int bulleScore = 0;
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        public int bulleScore;
+        public Transform finalPosition;
+
+        public GameObject bubblePrefab;
+
+        private void Start()
         {
-            
+            InvokeRepeating(nameof(SpawnBubble), 0f, 0.50f);
         }
 
-        // Update is called once per frame
-        void Update()
+        private void SpawnBubble()
         {
-        
+            GameObject newBubble = Instantiate(bubblePrefab, this.transform.position, Quaternion.identity);
+
+            // Récupère le script Bubble et lui transmet des valeurs
+            BulleScript bubbleScript = newBubble.GetComponent<BulleScript>();
+            if (bubbleScript != null)
+            {
+                bubbleScript.bulleManager = GetComponent<BulleManagerScript>();
+                bubbleScript.finalPosition = finalPosition;
+            }
         }
     }
 }
--- Assets/mehdi/scripts/BulleScript.cs
+++ Assets/mehdi/scripts/BulleScript.cs
@@ -1,36 +1,44 @@
 using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace mehdi.scripts
 {
     public class BulleScript : MonoBehaviour
     {
-        [SerializeField] private BulleManagerScript bulleManager; 
+        [SerializeField] public BulleManagerScript bulleManager; 
+        public Transform finalPosition;
+        private float _timer;
+        
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-        
+            Destroy(gameObject, 3.5f);
         }
 
         // Update is called once per frame
         void Update()
         {
-            throw new NotImplementedException();
-            // TODO
-            // remonte vers le haut puis disparait au bout de 3 secondes
+            transform.position = Vector3.MoveTowards(
+                transform.position, 
+                finalPosition.position, 
+                5f * Time.deltaTime
+            );
         }
 
         private void OnTriggerEnter(Collider other)
         {
+            Debug.Log("detects collision");
             if (other.gameObject.CompareTag("poisson"))
             {
-                if (other.gameObject.GetComponent<PoissonScript>().state)
-                {
-                    Destroy(this);
-                    bulleManager.bulleScore += 1;   
-                }
+                //if (other.gameObject.GetComponent<PoissonScript>().state)
+                //{
+                Debug.Log("is poisson");
+                bulleManager.GetComponent<BulleManagerScript>().bulleScore += 1;
+                Destroy(gameObject);
+                //}
             }
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
     }
 }
